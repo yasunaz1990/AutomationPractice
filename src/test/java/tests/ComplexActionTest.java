de33f3e8cf4c2b31b6a33a714bb4263fa4f90b7c
Nijat Muhtar <yasunaz1990@gmail.com>
2021-03-29 19:51:09 -0400

    First project image

---
+++ src/test/java/tests/ComplexActionTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ComplexActionTest {
+
+    @Test
+    public void testCaseOne() {
+        System.out.println("Hello there alphaleaf studetns!");
+    }
+}
