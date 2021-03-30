5422d812c0e11e98028c6fea90be9e96c9b973d6
Nijat Muhtar <yasunaz1990@gmail.com>
2021-03-29 21:08:02 -0400

    added one more test case

--- src/test/java/tests/ComplexActionTest.java
+++ src/test/java/tests/ComplexActionTest.java
@@ -10,4 +10,14 @@ public class ComplexActionTest {
         // I am on a main branch and typing this code
         System.out.println("Hello there alphaleaf studetns!");
     }
+
+    @Test
+    public void  testCaseTwo() {
+        // Comment - hello alphaleaf
+        // You guys rock! git branch is actually very hard concept
+        // to understand, just google it.
+        System.out.println("Git Branch is actually easy!");
+        System.out.println("Branch is separate version of project files & folders");
+    }
+
 }
